6e3f8de12e8cb29fd65e98917101caf5d1d28819
Tadahiro Yamamura <mt.village12@gmail.com>
2018-09-18 01:07:12 +0900

    debug用に付けた余計なprint文を削除

--- circle/database.py
+++ circle/database.py
@@ -5,4 +5,3 @@ from sqlalchemy.ext.declarative import declarative_base
 _engine = create_engine('sqlite:///database.sqlite', echo=True)
 
 Base = declarative_base(bind=_engine)
-print('database imported')
