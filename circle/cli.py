888148763695ff50571b6eff35591ca3a8149226
Tadahiro Yamamura <mt.village12@gmail.com>
2018-09-17 23:30:58 +0900

    まずはガワを作る

---
+++ circle/cli.py
@@ -0,0 +1,20 @@
+import click
+
+@click.group(invoke_without_command=False)
+def cli():
+  pass
+
+
+@cli.command(help='edit question')
+@click.option('--list', is_flag=True, help='show question list')
+@click.option('--add', '-a', help='add question')
+@click.option('--delete', '-d', help='remove question')
+def question(list, add, delete):
+  if list:
+    click.echo('list questions.')
+  elif add:
+    click.echo('question added: ' + add)
+  elif delete:
+    click.echo('question deleted: ' + delete)
+  else:
+    click.help
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+  setup(
+    name='circle',
+    version='1.0.0',
+    author='Tadahiro Yamamura',
+    license='MIT License',
+    install_requires=['click'],
+    packages=['circle'],
+    entry_points={
+      'console_scripts': ['circle = circle.cli:cli']
+    }
+  )
